02f9a9bbced02d5131446f1bf87df5ce1706b0a9
Digitalbullits <52849956+Digitalbullits@users.noreply.github.com>
2025-01-04 18:38:30 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,230 +1,52 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open-Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-DEVICE_PATH := device/google/lynx
-
-include device/google/gs201/BoardConfig-common.mk
-include vendor/google_devices/lynx/BoardConfigVendor.mk
-
-TARGET_BOARD_INFO_FILE := device/google/lynx/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := lynx
-
-# For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
-
-# 12.1 manifest requirements
-TARGET_SUPPORTS_64_BIT_APPS := true
-TARGET_IS_64_BIT := true
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
-PRODUCT_ENFORCE_VINTF_MANIFEST := true 
-DEVICE_MANIFEST_FILE := device/google/lynx/recovery/root/vendor/etc/vintf/manifest.xml
-
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := cortex-a55
-TARGET_CPU_VARIANT_RUNTIME := cortex-a55
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-
-# Enable 64-bit for non-zygote
-ZYGOTE_FORCE_64 := true
-
-# Force any prefer32 targets to be compiled as 64 bit.
-IGNORE_PREFER32_ON_DEVICE := true
-
-ENABLE_CPUSETS := true
-ENABLE_SCHEDBOOST := true
-
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := lynx
-TARGET_NO_BOOTLOADER := true
-
-# Display
-TARGET_SCREEN_DENSITY := 420
-TARGET_USES_VULKAN := true
-BOARD_EGL_CFG := device/google/gs201/conf/egl.cfg
-
-# EMULATOR common modules
-BOARD_EMULATOR_COMMON_MODULES := liblight
-
-# Kernel
-TARGET_KERNEL_DTBO_PREFIX := dts/
-TARGET_KERNEL_DTBO := google/devices/lynx/dtbo.img
-TARGET_KERNEL_DTB := \
-    google/devices/lynx/google-base/gs201-a0.dtb \
-    google/devices/lynx/google-base/gs201-b0.dtb \
-    google/devices/lynx/google-base/gs201-b0_v2-ipop.dtb
-
-# Kernel modules
-BOARD_VENDOR_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/lynx/recovery/root/vendor_dlkm.modules.load))
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/lynx/recovery/root/vendor_kernel_boot.modules.load))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
-BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
-BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := device/google/lynx/recovery/root/vendor_dlkm.modules.blocklist
-TARGET_KERNEL_EXT_MODULE_ROOT := kernel/google/gs201/private/google-modules
-
-TARGET_KERNEL_EXT_MODULES := \
-    aoc/usb \
-    bms \
-    display/samsung \
-    edgetpu/janeiro/drivers/edgetpu \
-    gpu/mali_kbase \
-    gpu/mali_pixel \
-    gxp/gs201 \
-    lwis \
-    power/reset \
-    touch/common \
-    touch/focaltech/ft3658 \
-    touch/goodix \
-    uwb/kernel \
-    video/gchips \
-    ../devices/google/lynx/display
-
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
-BOARD_SUPER_PARTITION_GROUPS := google_dynamic_partitions
-BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm
-BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
-
-VENDOR_CMDLINE := "dyndbg=\"func alloc_contig_dump_pages +p\" \
-                earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on \
-                swiotlb=noforce \
-		cma_sysfs.experimental=Y \
-		cgroup_disable=memory \
-		rcupdate.rcu_expedited=1 \
-		rcu_nocbs=all \
-		stack_depot_disable=off \
-		page_pinner=on \
-		swiotlb=1024 \
-		disable_dma32=on \
-                at24.write_timeout=100 \
-		log_buf_len=1024K \
-		bootconfig"
-
+# Enable load module in parallel
 BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
+
+# The modules which need to be loaded in sequential
+BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
 BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
 
-# Kernel
-BOARD_KERNEL_BASE        := 0x1000000
-BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_KERNEL_OFFSET      := 0x00008000
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_DTB_OFFSET         := 0x01f00000
+ifdef PHONE_CAR_BOARD_PRODUCT
+    include device/google_car/$(PHONE_CAR_BOARD_PRODUCT)/BoardConfig.mk
+else
+    TARGET_SCREEN_DENSITY := 420
+endif
 
-# vendor_boot as recovery
-BOARD_BOOT_HEADER_VERSION := 4
-BOARD_USES_RECOVERY_AS_BOOT := false
-BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
-BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := false
-BOARD_USES_GENERIC_KERNEL_IMAGE := true
-BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_IMAGE_NAME := Image.lz4
-TARGET_KERNEL_CONFIG := cloudripper_gki_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/gs201/private/gs-google
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
-BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtbs
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION) 
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(VENDOR_CMDLINE)
+TARGET_BOARD_INFO_FILE := device/google/pantah/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := panther
+BOARD_USES_GENERIC_AUDIO := true
+USES_DEVICE_GOOGLE_CLOUDRIPPER := true
+BOARD_KERNEL_CMDLINE += swiotlb=noforce
 
-# Platform
-TARGET_BOARD_PLATFORM := gs201
+include device/google/gs201/BoardConfig-common.mk
+-include vendor/google_devices/gs201/prebuilts/BoardConfigVendor.mk
+include device/google/gs-common/check_current_prebuilt/check_current_prebuilt.mk
+-include vendor/google_devices/panther/proprietary/BoardConfigVendor.mk
+include device/google/pantah/sepolicy/panther-sepolicy.mk
+include device/google/pantah/wifi/BoardConfig-wifi.mk
 
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-TARGET_RECOVERY_WIPE := device/google/lynx/recovery.wipe
-TARGET_RECOVERY_FSTAB := device/google/lynx/recovery.fstab
+ifneq (,$(RELEASE_ETM_IN_USERDEBUG_ENG))
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+-include device/google/common/etm/BoardUserdebugModules.mk
+endif
+endif
 
-# Ramdisk compression
-BOARD_RAMDISK_USE_LZ4 := true
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-
-# Enable chained vbmeta for boot images
-BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 2
-
-# Enable chained vbmeta for init_boot images
-BOARD_AVB_INIT_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_INIT_BOOT_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_INIT_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_INIT_BOOT_ROLLBACK_INDEX_LOCATION := 4
-
-# Hack: prevent anti rollback
-PLATFORM_VERSION := 14
-PLATFORM_SECURITY_PATCH := 2127-12-31
-PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
-VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
-BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
-
-# Load Touch modules files
-TW_LOAD_VENDOR_MODULES := "heatmap.ko touch_offload.ko ftm5.ko focal_touch.ko goodix_brl_touch.ko goog_touch_interface.ko"
-TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
-
-# TWRP specific build flags
-TWRP_EVENT_LOGGING := true
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-TW_THEME := portrait_hdpi
-TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
-TW_EXTRA_LANGUAGES := true
-TW_NO_SCREEN_BLANK := true
-TW_NO_SCREEN_TIMEOUT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_MAX_BRIGHTNESS := 520
-TW_INCLUDE_FASTBOOTD := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
-TW_USE_TOOLBOX := true
-TW_INCLUDE_LPDUMP := true
-TW_INCLUDE_LPTOOLS := true
-TW_INCLUDE_REPACKTOOLS := true
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_INCLUDE_RESETPROP := true
-TW_EXCLUDE_APEX := true
-TW_Y_OFFSET := 80
-TW_H_OFFSET := -80
-TW_OVERRIDE_SYSTEM_PROPS := \ 
-"ro.bootimage.build.date.utc=ro.build.date.utc;ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.name=ro.product.system.name"
+DEVICE_PATH := device/google/pantah
+VENDOR_PATH := vendor/google/panther
+include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigLineage.mk
+include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigEvolution.mk
